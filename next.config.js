50f4d9e7116c301bf13096ba085860504965a78e
Viet Hoang <ngviethoang0212@gmail.com>
2023-02-09 20:49:23 +0700

    test config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  httpAgentOptions: {
+    keepAlive: false,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
