7dfd9e4db2d9a59181320483a43386e04359de4b
Viet Hoang <ngviethoang0212@gmail.com>
2023-02-16 18:29:51 +0700

    analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import '@/styles/globals.css';
 import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
       <Component {...pageProps} />
+      <Analytics />
     </ChakraProvider>
   );
 }
